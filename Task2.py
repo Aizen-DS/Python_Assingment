8219011c0c259fd0a84a41b2a353d641b7293057
Aizen <parasdeshmukh2004@gmail.com>
2025-10-30 13:46:59 +0530

    Delete Task2.py

--- Task2.py
+++
@@ -1,9 +0,0 @@
-Numbers = list(range(1,11))
-
-first_five = Numbers[:5]
-
-reversed_list = first_five[::-1]
-
-print("Original list:",Numbers)
-print("Extracted list:",first_five)
-print("Revered list:",reversed_list)
