1af09355bb748b13e744243c867897ad68c87dac
Aizen <parasdeshmukh2004@gmail.com>
2025-10-30 14:05:23 +0530

    Add files via upload

---
+++ Assignment6/Task2.py
@@ -0,0 +1,9 @@
+Numbers = list(range(1,11))
+
+first_five = Numbers[:5]
+
+reversed_list = first_five[::-1]
+
+print("Original list:",Numbers)
+print("Extracted list:",first_five)
+print("Revered list:",reversed_list)
